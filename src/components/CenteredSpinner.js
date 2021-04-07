e05c6770ff3e728d91bbd0c176c40639aa33d21c
cdubone <cdubone@gmail.com>
2021-04-07 10:50:09 -0700

    CSS for centered spinner

--- src/components/CenteredSpinner.js
+++ src/components/CenteredSpinner.js
@@ -4,7 +4,7 @@ import Spinner from 'react-bootstrap/Spinner';
 const CenteredSpinner = () => {
 
     return (
-        <div className="d-flex justify-content-center">
+        <div className="centered-spinner">
             <Spinner animation="border" role="status" variant="secondary">
                 <span className="sr-only">Loading...</span>
             </Spinner>
